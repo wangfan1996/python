7b04b34b884887f57ccda552fc0ada26a68c0c0a
wangfan1996 <1356316925@qq.com>
2019-05-14 17:01:47 +0800

    requests练习

---
+++ requests/test1.py
@@ -0,0 +1,20 @@
+import requests, json, pymysql, time
+print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1aWQiOjEsImlhdCI6MTU1NzIxOTAyMSwiZXhwIjozMzU3MjE5MDIxfQ.hiyd_2WmhSf9oyf1VAFEur1CgroKg8efMgw6vZ7OKgU'
+headers = {'token':token}
+typeGet = requests.get("http://192.168.6.107/jwxslist/test",headers=headers)
+dictTypeGet = typeGet.json()
+data = dictTypeGet['data']
+item = []
+for x in data:
+    item.append(list(x.values()))
+db = pymysql.connect("118.89.242.170","root","wflyt5201314!@#","python" )
+cursor = db.cursor()
+sql = "INSERT INTO user(name,user,pwd,powerpwd,birth,qq,phone) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+try:
+   cursor.executemany(sql,item)
+   db.commit()
+except:
+   db.rollback()
+db.close()
+print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
