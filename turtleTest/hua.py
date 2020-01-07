1e8baaef3b01b7606d2628e29f93da046dae7d66
wangfan1996 <1356316925@qq.com>
2020-01-07 11:46:08 +0800

    turtle练习

---
+++ turtleTest/hua.py
@@ -0,0 +1,11 @@
+from turtle import *
+
+goto(100, 0)
+
+for i in range(50):
+    left(80)
+    fd(100)
+    left(135)
+    fd(105)
+
+done()
---
+++ turtleTest/wujiaoxing1.py
@@ -0,0 +1,9 @@
+import turtle as t
+
+t.goto(100, 0)
+
+for i in range(50):
+    t.left(80)
+    t.fd(100)
+    t.left(135)
+    t.fd(105)
